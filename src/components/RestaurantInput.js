39a6b31b4d1c6121bc0dbc9ada69ff4016bb7f97
JSONRete <jsonrete@gmail.com>
2021-10-18 16:18:27 -0400

    commit tree a04994a48bfced64e4cd575ae6bc87af60b4b189

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -23,7 +23,9 @@ export class RestaurantInput extends Component {
 
   handleOnSubmit = event => {
     event.preventDefault();
+    // console.log("R being added: ", this.state);
     // add missing code
+    this.props.addRestaurant(this.state);
   }
 
   render() {
@@ -51,4 +53,32 @@ export class RestaurantInput extends Component {
 
 
 //connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+// export default RestaurantInput
+// const addRestaurant = (state) => {
+//   return {
+//     restaurants: state.restaurants
+//   }
+// }
+
+// const RestaurantInput = (dispatch) => {
+//   return {
+//     addTodo: (todo) => {
+//       dispatch(addRestaurant(todo))
+//     }
+//   };
+// };
+
+// export default connect(null, addRestaurant)(RestaurantInput);
+
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addRestaurant: (newRestaurant) => {
+//       dispatch(addRestaurant(newRestaurant))
+//     }
+//   };
+// };
+
+
+// export default connect(null, mapDispatchToProps)(RestaurantInput);
+export default connect(null, { addRestaurant })(RestaurantInput);
